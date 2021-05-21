0a8ab2968c7c599d3e846ecdbfb7c4bba90e4973
jazzy8 <65190962+jazzy8@users.noreply.github.com>
2021-05-21 13:10:31 -0700

    Added the stuff for tracking.

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+// Set this to whatever number you like. This will be the transmit frequency.
+radio.setGroup(3)
+// You want max power transmission, this is a tracker!
+radio.setTransmitPower(7)
+basic.forever(function () {
+    // Repeat with a, b, and c. Three micro:bits in a triangle!
+    radio.sendValue("a", 1)
+    // You don't want to interfere with your other "satellites"
+    basic.pause(1000)
+})
